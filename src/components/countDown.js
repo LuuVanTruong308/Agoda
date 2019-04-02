1b43f6023278894dfcbe61fc1dac3ccbd6b35d5a
LuuVanTruong <luutruong308@gmail.com>
2019-04-03 00:54:01 +0700

    add count down

--- src/components/countDown.js
+++ src/components/countDown.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import Countdown, { zeroPadTime  } from 'react-countdown-now';
+import './countDown.scss'
+import Countdown from 'react-countdown-now';
 
-const renderer = ({ days, hours, minutes, seconds, completed }) => {
-    if (completed) {
-      return <span>00:00:00:00</span>;
-    } else {
-      return <span>{days}:{hours}:{minutes}:{seconds}</span>;
-    }
+const renderer = ({ days, hours, minutes, seconds }) => {
+  return <div className="count-down">
+    <div className="count-down-item">
+      <span>{days}</span>
+      <p>ngày</p>
+    </div>
+    <span>:</span>
+    <div className="count-down-item">
+      <span>{hours}</span>
+      <p>giờ</p>
+    </div>
+    <span>:</span>
+    <div className="count-down-item">
+      <span>{minutes}</span>
+      <p>phút</p>
+    </div>
+    <span>:</span>
+    <div className="count-down-item">
+      <span>{seconds}</span>
+      <p>giây</p>
+    </div>
+  </div>
 };
 
 class CountDown extends Component {
@@ -14,10 +31,10 @@ class CountDown extends Component {
     return (
       <div>
         <Countdown
-            date={Date.now() + 5000}
-            zeroPadTime={2}
-            renderer={renderer}
-        />,
+          date={Date.now() + 1000000}
+          zeroPadTime={2}
+          renderer={renderer}
+        />
     </div>
     );
   }
